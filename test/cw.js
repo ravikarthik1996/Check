97e9163aca5db0d99201b4e1c520635d3efb5ee2
Karthik <38332328+ravikarthik1996@users.noreply.github.com>
2020-06-04 23:28:18 -0400

    Delete cw.js

--- test/cw.js
+++
@@ -1,5 +0,0 @@
-const keccak256 = require('keccak256')
-var Web3 = require('web3');
-var web3 = new Web3();
-console.log(keccak256(1234567890).toString('hex'));
-console.log(keccak256('1234567890').toString('hex'));
